efe5d7e367148c7d31f15ebaa3da2985826027cd
Thomas Grusz <thomas.grusz@gmail.com>
2020-02-09 08:45:26 +0100

    feat: add operator overload for Vector class
    Add operator overload for __add__, __sub__, __mul__ .

--- linalg/vector.py
+++ linalg/vector.py
@@ -12,8 +12,23 @@ class Vector(object):
         except TypeError:
             raise TypeError("The coordinates must be an iterable!")
 
+    def __add__(self, other):
+        return Vector([c1 + c2 for c1, c2 in zip(self.coordinates, other.coordinates)])
+
+    def __sub__(self, other):
+        return Vector([c1 - c2 for c1, c2 in zip(self.coordinates, other.coordinates)])
+
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            return Vector(
+                [c1 * c2 for c1, c2 in zip(self.coordinates, other.coordinates)]
+            )
+        else:
+            return Vector([c * other for c in self.coordinates])
+
     def __repr__(self):
         return f"Vector: {self.coordinates}"
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+

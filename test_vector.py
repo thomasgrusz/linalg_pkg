389841c4115a695d4182f64c98823b8598b41864
Thomas Grusz <thomas.grusz@gmail.com>
2020-02-10 23:11:23 +0100

    test: update unittest

--- test_vector.py
+++ test_vector.py
@@ -14,6 +14,10 @@ class TestVectorClass(unittest.TestCase):
         self.v1 = Vector([8.218, -9.341])
         self.v2 = Vector([-1.129, 2.111])
         self.v3 = Vector([8.218, -9.341])
+        self.v4 = Vector([-2.328, -7.284, -1.214])
+        self.v5 = Vector([-1.821, 1.072, -2.94])
+        self.v6 = Vector([-7.579, -7.88])
+        self.v7 = Vector([22.737, 23.64])
 
     def test_initialization(self):
         self.assertEqual(
@@ -50,12 +54,12 @@ class TestVectorClass(unittest.TestCase):
     def test_magnitude(self):
         self.assertEqual(
             self.v1.magnitude(),
-            sqrt((Decimal(8.218) ** 2) + (Decimal(-9.341) ** 2)),
+            ((Decimal(8.218) ** 2) + (Decimal(-9.341) ** 2)) ** Decimal(0.5),
             "incorrect magnitude",
         )
 
     def test_normalized(self):
-        magnitude = Decimal(sqrt((Decimal(8.218) ** 2) + (Decimal(-9.341) ** 2)))
+        magnitude = ((Decimal(8.218) ** 2) + (Decimal(-9.341) ** 2)) ** Decimal(0.5)
         scalar = Decimal(1.0) / magnitude
 
         normalized_coordinates = ((Decimal(8.218) * scalar), (Decimal(-9.341) * scalar))
@@ -81,6 +85,39 @@ class TestVectorClass(unittest.TestCase):
         )
         self.assertEqual(
             self.v1.angle_with(self.v2, rad=False), angle_in_degrees, "incorrect angle"
+        )
+
+    def test_is_zero(self):
+        self.zero_vector = Vector([0, 0, 0])
+        self.assertEqual(
+            self.zero_vector.is_zero(), True, "incorrect, this is a zero vector"
+        )
+        self.assertEqual(
+            self.v1.is_zero(), False, "incorrect, this is NOT a zero vector"
+        )
+
+    def test_is_orthogonal_to(self):
+        self.assertEqual(
+            self.v4.is_orthogonal_to(self.v5),
+            True,
+            "incorrect result, these vectors are orthogonal to each other",
+        )
+        self.assertEqual(
+            self.v1.is_orthogonal_to(self.v2),
+            False,
+            "incorrect, these vectors are not orthogonal to each other",
+        )
+
+    def test_is_parallel_to(self):
+        self.assertEqual(
+            self.v1.is_parallel_to(self.v2),
+            False,
+            "incorrect, these vectors are NOT parallel to eaach other",
+        )
+        self.assertEqual(
+            self.v6.is_parallel_to(self.v7),
+            True,
+            "incorrect, these vectors are parallel to each other",
         )
 
     def test_repr(self):

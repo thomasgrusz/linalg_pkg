40d1a077685400511372b9bf87d1a6ebd50d1fbb
Thomas Grusz <thomas.grusz@gmail.com>
2020-02-10 17:23:06 +0100

    test: update unittest for vector class

--- test_vector.py
+++ test_vector.py
@@ -2,60 +2,90 @@
 # /usr/bin/python -m unittest test_vector
 
 import unittest
-
+from math import pi, sqrt
+from decimal import Decimal, getcontext
 from linalg.vector import Vector
+
+getcontext().prec = 30
 
 
 class TestVectorClass(unittest.TestCase):
     def setUp(self):
         self.v1 = Vector([8.218, -9.341])
         self.v2 = Vector([-1.129, 2.111])
-        self.v3 = self.v1
+        self.v3 = Vector([8.218, -9.341])
 
     def test_initialization(self):
-        self.assertEqual(self.v1.coordinates, (8.218, -9.341), "incorrect coordinates")
+        self.assertEqual(
+            self.v1.coordinates,
+            (Decimal(8.218), Decimal(-9.341)),
+            "incorrect coordinates",
+        )
         self.assertEqual(self.v1.dimension, 2, "incorrect dimension")
 
     def test_add(self):
         self.added = self.v1 + self.v2
-        self.assertEqual(self.added.coordinates, (7.089, -7.23), "incorrect summation")
+        self.assertEqual(
+            self.added.coordinates,
+            ((Decimal(8.218) + Decimal(-1.129)), (Decimal(-9.341) + Decimal(2.111))),
+            "incorrect summation",
+        )
 
     def test_subract(self):
         self.subtracted = self.v1 - self.v2
         self.assertEqual(
-            self.subtracted.coordinates, (9.347, -11.452), "incorrect subtraction"
+            self.subtracted.coordinates,
+            ((Decimal(8.218) - Decimal(-1.129)), (Decimal(-9.341) - Decimal(2.111))),
+            "incorrect subtraction",
         )
 
     def test_times_scalar(self):
         self.multiplied_with_scalar = self.v1.times_scalar(7.41)
         self.assertEqual(
             self.multiplied_with_scalar.coordinates,
-            (60.895, -69.217),
+            ((Decimal(8.218) * Decimal(7.41)), (Decimal(-9.341) * Decimal(7.41))),
             "incorrect subtraction",
         )
 
     def test_magnitude(self):
-        self.assertEqual(self.v1.magnitude(), 12.441, "incorrect magnitude")
+        self.assertEqual(
+            self.v1.magnitude(),
+            sqrt((Decimal(8.218) ** 2) + (Decimal(-9.341) ** 2)),
+            "incorrect magnitude",
+        )
 
     def test_normalized(self):
+        magnitude = Decimal(sqrt((Decimal(8.218) ** 2) + (Decimal(-9.341) ** 2)))
+        scalar = Decimal(1.0) / magnitude
+
+        normalized_coordinates = ((Decimal(8.218) * scalar), (Decimal(-9.341) * scalar))
         self.assertEqual(
-            self.v1.normalized().coordinates, (0.661, -0.751), "incorrect normalization"
+            self.v1.normalized().coordinates,
+            normalized_coordinates,
+            "incorrect normalization",
         )
 
     def test_dot(self):
-        self.assertEqual(self.v1.dot(self.v2), -28.997, "incorrect dot product")
+        dot_product = (Decimal(8.218) * Decimal(-1.129)) + (
+            Decimal(-9.341) * Decimal(2.111)
+        )
+        self.assertEqual(self.v1.dot(self.v2), dot_product, "incorrect dot product")
 
     def test_angle_with(self):
+
+        angle_in_rads = 2.9111755112971575698566084611229598522186279296875
+        angle_in_degrees = angle_in_rads * 180.0 / pi
+
         self.assertEqual(
-            self.v1.angle_with(self.v2, rad=True), 2.913, "incorrect angle"
+            self.v1.angle_with(self.v2, rad=True), angle_in_rads, "incorrect angle"
         )
         self.assertEqual(
-            self.v1.angle_with(self.v2, rad=False), 166.906, "incorrect angle"
+            self.v1.angle_with(self.v2, rad=False), angle_in_degrees, "incorrect angle"
         )
 
-    def test_str(self):
+    def test_repr(self):
         self.assertEqual(
-            self.v1.__str__(), "Vector: (8.218, -9.341)", "incorrect string"
+            self.v1.__repr__(), "Vector: [8.218, -9.341]", "incorrect string"
         )
 
     def test_equal(self):
